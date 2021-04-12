db0c0d5853c3eae83257f7e1ca7f9c1aa7ea48eb
2280727 <69791798+2280727@users.noreply.github.com>
2021-04-12 07:52:54 +0200

    Registration logic update

--- app/src/main/java/com/example/kingpins/RegistrationActivity.java
+++ app/src/main/java/com/example/kingpins/RegistrationActivity.java
@@ -149,7 +149,7 @@ public class RegistrationActivity extends AppCompatActivity {
             alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    password.setText("");
+                    etPassword.setText("");
                     dialog.dismiss();
                 }
             });
