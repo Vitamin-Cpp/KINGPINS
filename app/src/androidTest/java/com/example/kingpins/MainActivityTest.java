1ac14cee0166ec9e407c9362b1da5a18d9fa0073
thapelo <thapelomgp@gmail.com>
2021-04-27 17:03:46 +0200

    Test views on Login screen

--- app/src/androidTest/java/com/example/kingpins/MainActivityTest.java
+++ app/src/androidTest/java/com/example/kingpins/MainActivityTest.java
@@ -3,6 +3,7 @@ package com.example.kingpins;
 import android.widget.Button;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -17,6 +18,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityTest {
 
     @Rule
