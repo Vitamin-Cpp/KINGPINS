2afe43556a449eeb0dfb29ba7750935182d0a412
katlehojnr3520 <katlehojnr3520@gmail.com>
2021-05-10 00:23:56 +0200

    Home page unit testing

---
+++ app/src/androidTest/java/com/example/kingpins/MainActivityTest.java
@@ -0,0 +1,47 @@
+package com.example.kingpins;
+
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule <MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+    private MainActivity mainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+    mainActivity = mainActivityActivityTestRule.getActivity();
+    }
+
+    @Test
+    public void onCreate() {
+        View view = mainActivity.findViewById(R.id.layout_loginEmail);
+        assertNotNull(view);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mainActivity = null;
+    }
+
+    @Test
+    public void validationErrors() {
+    }
+
+    @Test
+    public void doLogin() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+}
