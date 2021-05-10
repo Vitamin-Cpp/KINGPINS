40f0ba50314058ea935c0180e8819b05184fc575
thapelo <thapelomgp@gmail.com>
2021-05-10 04:35:40 +0200

    Make User details accessible via Constants

--- app/src/main/java/constants_classes/Constants.java
+++ app/src/main/java/constants_classes/Constants.java
@@ -6,4 +6,8 @@ public class Constants
 
     // name of the shared preferences
     public static final String URL_LINK = "https://lamp.ms.wits.ac.za/home/s2280727/kingpins/";
+    public static String USER_EMAIL = "";
+    public static String USER_FIRST_NAME = "";
+    public static String USER_LAST_NAME = "";
+    public static String USER_FUNDS = "";
 }
