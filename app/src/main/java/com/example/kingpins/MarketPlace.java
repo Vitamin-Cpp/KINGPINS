392ed3411dfac6a918c1447a5b7a4e28bee0302b
2280727 <69791798+2280727@users.noreply.github.com>
2021-05-20 16:21:05 +0200

    Tablayout for MarketPlace

--- app/src/main/java/com/example/kingpins/MarketPlace.java
+++ app/src/main/java/com/example/kingpins/MarketPlace.java
@@ -2,18 +2,61 @@ package com.example.kingpins;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MarketPlace extends AppCompatActivity {
     DrawerLayout drawerLayout;
+    TabLayout tabLayout;
+    ViewPager2 pager2;
+    FragmentAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_place);
+
         drawerLayout = findViewById(R.id.drawer_layout);
+        tabLayout=findViewById(R.id.tab_layout);
+        pager2=findViewById(R.id.view_pager);
+
+        FragmentManager fm=getSupportFragmentManager();
+        adapter=new FragmentAdapter(fm, getLifecycle());
+        pager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Products"));
+        tabLayout.addTab(tabLayout.newTab().setText("Services"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+
     }
+
     public void ClickMenu(View view){
         //open the drawer
         Homepage.openDrawer(drawerLayout);
