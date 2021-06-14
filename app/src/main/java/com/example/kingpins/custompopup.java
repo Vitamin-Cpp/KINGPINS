fe51cffe915845562c2ac45b33f89f7ec6ff6796
2280727 <69791798+2280727@users.noreply.github.com>
2021-06-14 13:40:09 +0200

    Upload product and service

--- app/src/main/java/com/example/kingpins/custompopup.java
+++ app/src/main/java/com/example/kingpins/custompopup.java
@@ -16,13 +16,17 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,30 +39,34 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
+import java.*;
 
 import connection_classes.PHPrequest;
 import connection_classes.RequestHandler;
+import constants_classes.Constants;
 import okhttp3.HttpUrl;
 
+//import static constants_classes.Constants.USER_EMAIL;
 
-public class custompopup extends AppCompatActivity {
 
-    private ImageView selectedImage;
+public class custompopup extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    ImageView imageView;
+    private Spinner category;
     private TextView txtclose;
-    public EditText productName, productPrice, category,Email;
+    public EditText productName, productPrice;
     private Button btnUpload;
     private RadioButton radProduct,radService;
     // Activity getApplicationContext();
 
-    private Bitmap bitmap;
+    private String choiceOfCategory="";
     private String KEY_IMAGE = "image";
     private String KEY_NAME = "productName";
     private String KEY_PRICE = "productPrice";
@@ -74,15 +82,17 @@ public class custompopup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custompopup);
-        selectedImage = findViewById(R.id.selectediImage);
+        imageView=  findViewById(R.id.selectediImage);
         txtclose = (TextView) findViewById(R.id.txtclose);
         btnUpload = (Button) findViewById(R.id.btnUpload);
         productName = findViewById(R.id.productName);
         productPrice = findViewById(R.id.productPrice);
-        category = findViewById(R.id.category);
+        category = findViewById(R.id.spCategory);
         radProduct = (RadioButton) findViewById(R.id.rdProduct);
         radService = (RadioButton)findViewById(R.id.rdService);
-        Email= findViewById(R.id.Email);
+
+
+
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,199 +105,213 @@ public class custompopup extends AppCompatActivity {
         findViewById(R.id.button_choose_image).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(custompopup.this, new String[]{
-                                    Manifest.permission.READ_EXTERNAL_STORAGE},
-                            REQUEST_CODE_SELECT_IMAGE
-                    );
-                }else {
+                //check condition
+                if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    //When permission denied
+                    //request permission
+                    ActivityCompat.requestPermissions(custompopup.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_STORAGE_PERMISSION);
+                }
+                else {
+                    //when permission grated
+                    //create method
                     selectImage();
                 }
 
 
             }
         });
-        //this is the button we use to send the image and all the contents of our add to the database
-//        btnUpload.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //we send stuff for storage on the database
-//                if (bitmap==null)
-//                {
-//                    Toast.makeText(getApplicationContext(),"Please Upload Image",Toast.LENGTH_SHORT).show();
-//                }
-//                else {
-//                    PHPrequest phPrequest=new PHPrequest();
-//                    phPrequest.doRequest(custompopup.this, "upload_product.php", new RequestHandler() {
-//
-//                        // do request
-//                        @Override
-//                        public HttpUrl.Builder passParametersToURL(HttpUrl.Builder urlBuilder)
-//                        {
-//
-//                            String image = getStringImage(bitmap);
-//
-//                            //Getting Image Name
-//                            String name1 = productName.getText().toString().trim();
-//                            String category1 = category.getText().toString().trim();
-//                            String producprice1 = productPrice.getText().toString().trim();
-//                            // override method to pass relevant parameters
-//                            urlBuilder.addQueryParameter(KEY_IMAGE, image);
-//                            urlBuilder.addQueryParameter(KEY_NAME, name1);
-//                            urlBuilder.addQueryParameter(KEY_PRICE,producprice1);
-//                            urlBuilder.addQueryParameter(KEY_CATEGORY,category1);
-//                            urlBuilder.addQueryParameter("seller", "Phumlani@Ntini.com");
-//
-//                            return urlBuilder;
-//                        }
-//
-//                        @Override
-//                        public void processResponse(String responseFromRequest)
-//                        {
-//                            // override method to process response from server
-//                            useResponse(responseFromRequest);
-//                        }
-//                    });
-//
-//                }
-//
-//
-//
-//            }
-//
-//
-//        });
+        Spinner spinner = findViewById(R.id.spCategory);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
 
 
     }
 
     public void doUpload(View view)
     {
-        if(radProduct.isChecked()){
-        if (bitmap==null)
+        if(choiceOfCategory.isEmpty()||choiceOfCategory.equals("Category")) {
+            Toast.makeText(getApplicationContext(), "Please select category", Toast.LENGTH_LONG).show();
+        }else if(!radService.isChecked()&&!radProduct.isChecked())
         {
-            Toast.makeText(getApplicationContext(),"Please Upload Image",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Please select Service or Product", Toast.LENGTH_LONG).show();
         }
-        else {
-            PHPrequest phPrequest=new PHPrequest();
+        else{
+        if (radProduct.isChecked()) {
+
+            PHPrequest phPrequest = new PHPrequest();
             phPrequest.doRequest(custompopup.this, "upload_product.php", new RequestHandler() {
 
                 // do request
                 @Override
-                public HttpUrl.Builder passParametersToURL(HttpUrl.Builder urlBuilder)
-                {
+                public HttpUrl.Builder passParametersToURL(HttpUrl.Builder urlBuilder) {
 
-                   // String image =getStringImage(bitmap);
+                    String image = imageToString();
 
-                    //Getting Image Name
+
                     String name1 = productName.getText().toString().trim();
-                    String category1 = category.getText().toString().trim();
+
                     String producprice1 = productPrice.getText().toString().trim();
                     // override method to pass relevant parameters
-                    //urlBuilder.addQueryParameter(KEY_IMAGE, image);
+                    urlBuilder.addQueryParameter(KEY_IMAGE, image);
                     urlBuilder.addQueryParameter(KEY_NAME, name1);
-                    urlBuilder.addQueryParameter(KEY_PRICE,producprice1);
-                    urlBuilder.addQueryParameter(KEY_CATEGORY,category1);
-                    Intent intent=getIntent();
+                    urlBuilder.addQueryParameter(KEY_PRICE, producprice1);
+                    urlBuilder.addQueryParameter(KEY_CATEGORY,choiceOfCategory);
+                    Intent intent = getIntent();
 
-                    urlBuilder.addQueryParameter("seller", Email.getText().toString().trim());
+                    urlBuilder.addQueryParameter("seller",Constants.USER_EMAIL);
 
                     return urlBuilder;
                 }
 
                 @Override
-                public void processResponse(String responseFromRequest)
-                {
+                public void processResponse(String responseFromRequest) {
                     // override method to process response from server
                     useResponse(responseFromRequest);
                 }
             });
+            //saveChanges();
 
-        }
-        }else if(radService.isChecked()){
-            PHPrequest phPrequest=new PHPrequest();
+
+        } else if (radService.isChecked()) {
+            PHPrequest phPrequest = new PHPrequest();
             phPrequest.doRequest(custompopup.this, "upload_service.php", new RequestHandler() {
 
                 // do request
                 @Override
-                public HttpUrl.Builder passParametersToURL(HttpUrl.Builder urlBuilder)
-                {
+                public HttpUrl.Builder passParametersToURL(HttpUrl.Builder urlBuilder) {
 
                     // String image =getStringImage(bitmap);
 
                     //Getting Image Name
                     String name1 = productName.getText().toString().trim();
-                    String category1 = category.getText().toString().trim();
+                    //String category1 = category.getText().toString().trim();
                     String producprice1 = productPrice.getText().toString().trim();
                     // override method to pass relevant parameters
                     //urlBuilder.addQueryParameter(KEY_IMAGE, image);
                     urlBuilder.addQueryParameter(KEY_NAME, name1);
-                    urlBuilder.addQueryParameter(KEY_PRICE,producprice1);
-                    urlBuilder.addQueryParameter(KEY_CATEGORY,category1);
-                    Intent intent=getIntent();
+                    urlBuilder.addQueryParameter(KEY_PRICE, producprice1);
+                    //   urlBuilder.addQueryParameter(KEY_CATEGORY,category1);
+                    Intent intent = getIntent();
 
-                    urlBuilder.addQueryParameter("seller",  Email.getText().toString().trim());
+                    urlBuilder.addQueryParameter("seller", Constants.USER_EMAIL);
 
                     return urlBuilder;
                 }
 
                 @Override
-                public void processResponse(String responseFromRequest)
-                {
+                public void processResponse(String responseFromRequest) {
                     // override method to process response from server
                     useResponse(responseFromRequest);
                 }
             });
-
-        }
-
-
-        }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if(requestCode==REQUEST_CODE_STORAGE_PERMISSION && grantResults.length>0){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                selectImage();
-            }else{
-                Toast.makeText(this, "Permission Denied",Toast.LENGTH_SHORT).show();
-            }
         }
     }
+
+        }
+
+    private void selectImage() {
+        //clear previous data
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_CODE_SELECT_IMAGE);
+
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        choiceOfCategory= parent.getItemAtPosition(position).toString();
+        //Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //check condition
+        if(requestCode==REQUEST_CODE_STORAGE_PERMISSION && grantResults.length== PackageManager.PERMISSION_GRANTED && grantResults.length > 0){
+            //when permission is granted
+            //call method
+            selectImage();
+        }
+        else{
+            //when permission is denied
+            Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_CODE_SELECT_IMAGE &&  resultCode == RESULT_OK){
-            if(data != null){
-                Uri selectedImageUri = data.getData();
-                if(selectedImageUri != null){
-                    try{
-                        InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
-                        bitmap = BitmapFactory.decodeStream(inputStream);
-                        selectedImage.setImageBitmap(bitmap);
-
-
-                        //the file path of the selected image
-                        //File selectedImagefile= new File(getPathFromUri(selectedImageUri));
-
-                    }catch (Exception exception){
-                        Toast.makeText(this,exception.getMessage(),Toast.LENGTH_SHORT).show();
-                    }
+        //check condition
+        if(requestCode==REQUEST_CODE_SELECT_IMAGE && resultCode==RESULT_OK && data!= null){
+            //When result is ok
+            //Initialize uri
+            Uri selectedImageUri= data.getData();
+            if(selectedImageUri != null){
+                try {
+                    InputStream inputStream= getContentResolver().openInputStream(selectedImageUri);
+                    Bitmap bitmap= BitmapFactory.decodeStream(inputStream);
+                    imageView.setImageBitmap(bitmap);
+                }catch (Exception exception){
+                    Toast.makeText(this, exception.getMessage(),Toast.LENGTH_SHORT).show();
                 }
             }
+
         }
+    }
+    private void saveChanges(){
+        StringRequest request= new StringRequest(Request.Method.POST, Constants.URL_LINK + "upload_product.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(),response.toString(),Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Nullable
+            @org.jetbrains.annotations.Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params= new HashMap<>();
+                params.put("productName", productName.getText().toString());
+                //params.put("productPrice", productPrice.getText().toString();
+                params.put("seller", Constants.USER_EMAIL);
+                params.put("category", choiceOfCategory);
+                params.put("image", imageToString());
+                return params;
+            }
+        };
+
+        RequestQueue queue= Volley.newRequestQueue(getApplicationContext());
+        queue.add(request);
     }
 
-    private void selectImage(){
-        selectedImage.setImageBitmap(null);
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        if(intent.resolveActivity(getPackageManager())!=null){
-            startActivityForResult(intent,REQUEST_CODE_SELECT_IMAGE);
-        }
+    private String imageToString()
+    {
+        imageView.buildDrawingCache();
+        Bitmap bitmap=imageView.getDrawingCache();
+        ByteArrayOutputStream stream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+        byte[] image=stream.toByteArray();
+        return Base64.encodeToString(image,0);
     }
+
+
+
+
+
 
     private void useResponse(String response) {
         // php successfully connected to db and found user with correct detail
@@ -329,13 +353,6 @@ public class custompopup extends AppCompatActivity {
         }
     }
 
-    public String getStringImage(Bitmap bmp){
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        byte[] imageBytes = baos.toByteArray();
-        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
-        return encodedImage;
-    }
 
 
 
