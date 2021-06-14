66249cbb72d487233937336e7d1de11562de82a2
thapelo <thapelomgp@gmail.com>
2021-06-15 00:44:31 +0200

    Try testing

--- app/src/androidTest/java/com/example/kingpins/MainActivityTest.java
+++ app/src/androidTest/java/com/example/kingpins/MainActivityTest.java
@@ -1,45 +1,45 @@
-package com.example.kingpins;
-
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.filters.LargeTest;
-
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
-@RunWith(AndroidJUnit4.class)
-@LargeTest
-public class MainActivityTest {
-
-    @Rule
-    public ActivityScenarioRule<MainActivity> activityRule
-            = new ActivityScenarioRule<>(MainActivity.class);
-    String strEmail, strPassword;
-    @Before
-    public void initValidString() {
-        // give variables values
-        strEmail = "thapelo@wits.ac.za";
-        strPassword = "12345678";
-    }
-
-    @Test
-    public void checkViewsAppeared() {
-        onView(withId(R.id.txtSlogan)).check(matches(isDisplayed()));
-        onView(withId(R.id.loginEmail)).check(matches(isDisplayed()));
-        onView(withId(R.id.loginPassword)).check(matches(isDisplayed()));
-        onView(withId(R.id.btnLogin)).check(matches(isDisplayed()));
-        onView(withId(R.id.btnRegister)).check(matches(isDisplayed()));
-        onView(withId(R.id.btnGoogleLogin)).check(matches(isDisplayed()));
-    }
-
-}
+//package com.example.kingpins;
+//
+//import org.junit.Before;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//
+//import androidx.test.ext.junit.rules.ActivityScenarioRule;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+//import androidx.test.filters.LargeTest;
+//
+//import static androidx.test.espresso.Espresso.onView;
+//import static androidx.test.espresso.action.ViewActions.click;
+//import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+//import static androidx.test.espresso.action.ViewActions.typeText;
+//import static androidx.test.espresso.assertion.ViewAssertions.matches;
+//import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+//import static androidx.test.espresso.matcher.ViewMatchers.withId;
+//
+//@RunWith(AndroidJUnit4.class)
+//@LargeTest
+//public class MainActivityTest {
+//
+//    @Rule
+//    public ActivityScenarioRule<MainActivity> activityRule
+//            = new ActivityScenarioRule<>(MainActivity.class);
+//    String strEmail, strPassword;
+//    @Before
+//    public void initValidString() {
+//        // give variables values
+//        strEmail = "thapelo@wits.ac.za";
+//        strPassword = "12345678";
+//    }
+//
+//    @Test
+//    public void checkViewsAppeared() {
+//        onView(withId(R.id.txtSlogan)).check(matches(isDisplayed()));
+//        onView(withId(R.id.loginEmail)).check(matches(isDisplayed()));
+//        onView(withId(R.id.loginPassword)).check(matches(isDisplayed()));
+//        onView(withId(R.id.btnLogin)).check(matches(isDisplayed()));
+//        onView(withId(R.id.btnRegister)).check(matches(isDisplayed()));
+//        onView(withId(R.id.btnGoogleLogin)).check(matches(isDisplayed()));
+//    }
+//
+//}
